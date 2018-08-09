5931cf42a291f0cf6549e07765d589675a7e1579
Jacob Garcia <janikgar@gmail.com>
2018-08-09 14:45:17 -0500

    add spec and skeleton of route schedule updater

--- app/jobs/get_route_schedule_job.rb
+++ app/jobs/get_route_schedule_job.rb
@@ -2,6 +2,15 @@ class GetRouteScheduleJob < ApplicationJob
   queue_as :default
 
   def perform(route_string)
-    # Do something later
+    
+    remote_test = HTTParty.get("https://transit.land/api/v1/schedule_stop_pairs?date=today&operator_onestop_id=o-9vrf-neworleansrta&route_onestop_id=#{route_string}&per_page=1")
+
+  end
+
+  def schedule_exist?(route_string)
+    if Schedule.count != 0
+      find_schedule = Route.where(route: route_string).pluck(remote_updated_at)
+    else
+      return false
   end
 end
--- spec/jobs/get_route_schedule_job_spec.rb
+++ spec/jobs/get_route_schedule_job_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe GetRouteScheduleJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe GetRouteScheduleJob, type: :job do
+  pending "This job does not exist yet"
+  # it "checks to see if there is local route schedule info in the db" do
+  #   expect
+  # end
 end
