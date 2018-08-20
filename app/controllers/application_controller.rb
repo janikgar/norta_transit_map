50b8e5b747d7100c297a3195c928236107b347d5
janikgar <janikgar@gmail.com>
2018-08-20 13:31:12 -0500

    reset Devise to work with Facebook correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || root_path
+  end
+
   private
   def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   # root 'platform#view'
   root 'platform#home'
   get 'system_map' => 'platform#view'
   get 'route_map' => 'platform#route'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'auth/:provider/callback' => 'sessions#create'
+  # get 'auth/:provider/callback' => 'sessions#create'
   get '/signout' => 'sessions#destroy', as: :signout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
